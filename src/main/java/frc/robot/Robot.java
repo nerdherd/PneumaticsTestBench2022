cb699918aa42b3d1f35ea2d03c49494be8efa325
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-02-04 20:46:20 -0800

    Report air compressor to smart dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -76,6 +76,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    compressor.reportToSmartDashboard();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
--- src/main/java/frc/robot/subsystems/AirCompressor.java
+++ src/main/java/frc/robot/subsystems/AirCompressor.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.CompressorConfigType;
 import edu.wpi.first.util.sendable.SendableBuilder;
 
@@ -64,4 +65,10 @@ public class AirCompressor extends SubsystemBase {
     public void initSendable(SendableBuilder sendableBuilder) {
         m_compressor.initSendable(sendableBuilder);
     }   
+
+    public void reportToSmartDashboard() {
+        SmartDashboard.putNumber("Air Compressor Analog Voltage", getAnalogVoltage());
+        SmartDashboard.putNumber("Air Compressor Current", getCurrent());
+        SmartDashboard.putNumber("Air Compressor Pressure", getPressure());
+    }
 }
